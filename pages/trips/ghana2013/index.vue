1290a27568b7e675afb35a52722764c26b34aeda
Mark Ahrens <mark.ahrens@acquia.com>
2021-09-08 15:18:22 -0500

    adding initial trips work

---
+++ pages/trips/ghana2013/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <article>
+    <h1>Ghana 2013</h1>
+    <ul>
+      <li v-for="post of posts" :key="post.slug">
+            <h2>{{ post.title }}</h2>
+      </li>
+    </ul>
+  </article>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const posts = await $content('trips/ghana2013')
+        .only(['title'])
+        .fetch()
+
+      return {
+        posts
+      }
+    }
+  }
+</script>
---
+++ pages/trips/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <article>
+    <h1>Trips</h1>
+    <div>
+      <div><NuxtLink to="/trips/ghana2013">Ghana 2013</NuxtLink></div>
+      <div>Ghana 2015</div>
+      <div>Haiti 2017</div>
+    </div>
+  </article>
+</template>
