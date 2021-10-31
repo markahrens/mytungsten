dece8fcc200562bc314f96554222094c14298479
Mark Ahrens <themarkahrens@live.com>
2021-10-31 13:12:47 -0500

    Switching to Projects

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,7 +2,7 @@
     <nav>
         <a href="/" id="site-title">MyTungsten</a>
         <ul>
-            <li><a href="https://bowls.mytungsten.net">Bowls</a></li>
+            <li><NuxtLink to="/projects">Projects</NuxtLink></li>
             <li><NuxtLink to="/trips">Trips</NuxtLink></li>
             <li><a href="/">Blog</a></li>
         </ul>
